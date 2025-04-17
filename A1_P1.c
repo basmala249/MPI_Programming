7051f0efc958171c33d934172e2295718aac3f2e
Maya Fouad <112335115+mayafouad@users.noreply.github.com>
2025-04-17 15:10:20 +0200

    Add files via upload

---
+++ A1_P1.c
@@ -0,0 +1,70 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    int rank, size, n, *array = NULL;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank); 
+    MPI_Comm_size(MPI_COMM_WORLD, &size); 
+
+    if (rank == 0) {
+        printf("Hello from master process.\n");
+        printf("Number of slave processes is %d\n", size - 1);
+        printf("Please enter size of array\n");
+        scanf("%d", &n);
+
+        array = (int*)malloc(n * sizeof(int));
+        printf("Please enter array elements\n");
+        for (int i = 0; i < n; i++)
+            scanf("%d", &array[i]);
+
+        int base = n / (size - 1), rem = n % (size - 1), start = 0;
+
+        for (int i = 1; i < size; i++) {
+            int count = base + (i <= rem ? 1 : 0);
+            MPI_Send(&count, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(array + start, count, MPI_INT, i, 0, MPI_COMM_WORLD);
+            start += count;
+        }
+
+        int global_max = array[0], global_index = 0, max_val, idx, offset = 0;
+        start = 0;
+        for (int i = 1; i < size; i++) {
+            int count = base + (i <= rem ? 1 : 0);
+            MPI_Recv(&max_val, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Recv(&idx, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            if (max_val > global_max) {
+                global_max = max_val;
+                global_index = start + idx;
+            }
+            start += count;
+        }
+
+        printf("Master process announce the final max which is %d and its index is %d.\n", global_max, global_index);
+        free(array);
+    } 
+    else {
+        int count;
+        MPI_Recv(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        int* sub_array = (int*)malloc(count * sizeof(int));
+        MPI_Recv(sub_array, count, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        int max = sub_array[0], index = 0;
+        for (int i = 1; i < count; i++) {
+            if (sub_array[i] > max) {
+                max = sub_array[i];
+                index = i;
+            }
+        }
+
+        printf("Hello from slave#%d Max number in my partition is %d and index is %d.\n", rank, max, index);
+        MPI_Send(&max, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        MPI_Send(&index, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        free(sub_array);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ A1_P2.c
@@ -0,0 +1,69 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void caesar(char *str, int n, int shift) {
+    for (int i = 0; i < n; i++) {
+        str[i] = str[i] + shift;
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size, len, shift;
+    char *text = NULL;
+    char mode;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        // MASTER
+        printf("Enter 'e' to encrypt or 'd' to decrypt: ");
+        fflush(stdout);
+        scanf(" %c", &mode);
+        shift = (mode == 'e') ? 3 : -3;
+
+        printf("Enter string (max 100): ");
+        fflush(stdout);
+        char input[101];
+        scanf("%s", input);
+        len = strlen(input);
+        text = input;
+        int base = len / (size - 1), rem = len % (size - 1), start = 0;
+        for (int i = 1; i < size; i++) {
+            int count = base + (i <= rem ? 1 : 0);
+            MPI_Send(&count, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            MPI_Send(text + start, count, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+            MPI_Send(&shift, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            start += count;
+        }
+        char *result = (char *)malloc(len + 1);
+        start = 0;
+        for (int i = 1; i < size; i++) {
+            int count = base + (i <= rem ? 1 : 0);
+            MPI_Recv(result + start, count, MPI_CHAR, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            start += count;
+        }
+        result[len] = '\0';
+        printf("Result: %s\n", result);
+        fflush(stdout);
+        free(result);
+    } 
+    else {
+        int count;
+        MPI_Recv(&count, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        char *sub = (char *)malloc(count);
+        MPI_Recv(sub, count, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Recv(&shift, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        caesar(sub, count, shift);
+        printf("Hello from slave #%d: processed %d characters.\n", rank, count);
+        fflush(stdout);
+        MPI_Send(sub, count, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+        free(sub);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
